3adc3d52c166c1869ae01a3a74b77575f874d0e4
Roshadgu <roshadgu@gmail.com>
2021-04-22 18:41:01 -0400

    Deleted some duplicate resources.

--- app/src/main/java/com/lunar/treehouse/MainActivity.kt
+++ app/src/main/java/com/lunar/treehouse/MainActivity.kt
@@ -2,13 +2,18 @@ package com.lunar.treehouse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import com.roshadgu.treehouse.components.HomeActivity
 
 class MainActivity : AppCompatActivity()
 {
   override fun onCreate(savedInstanceState: Bundle?)
   {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContent{
+      Text(text = "Hello World")
+    }
 
   }
 }
